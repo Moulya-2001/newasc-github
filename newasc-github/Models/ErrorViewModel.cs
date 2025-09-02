2f6cae64f3fb44ac76f1d112e24a6e0a946ab225
ram1 <ram1.java@gmail.com>
2025-09-02 09:56:18 +0530

    Add project files.

---
+++ newasc-github/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace newasc_github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
